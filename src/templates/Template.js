db3d8513e4a105d106fc060891e6dba47979e8fa
Jean Magic <jeancarlos70@gmail.com>
2021-03-24 10:52:57 -0400

    production

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -26,13 +26,13 @@ const Template = async () => {
           </ul>
         </div>
         <div class="card_social">
-          <a href="https://twitter.com/gndx">
+          <a href="https://twitter.com/MagoJeanMaster">
             <img src="${twitter}" />
           </a>
-          <a href="https://github.com/gndx">
+          <a href="https://github.com/JeanMaster">
             <img src="${github}" />
           </a>
-          <a href="https://instagram.com/gndx">
+          <a href="https://www.instagram.com/jean.hallak/">
             <img src="${instagram}" />
           </a>
         </div>
